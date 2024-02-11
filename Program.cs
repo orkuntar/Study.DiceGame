987dbb6847e2f4ccec3b934f2faa6d138385abad
Orkun Tarhan <orkuntar@gmail.com>
2024-02-11 16:17:10 +0300

    cleaner

--- Program.cs
+++ Program.cs
@@ -1,33 +1,42 @@
-﻿using System;
-Random dice = new Random();
+﻿Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
+
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine($"You rolled doubles! +2 bonus to total!");
-    total += 2;
-    
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-if ((roll1 == roll3) && (roll1 == roll3 ))
+if (total >= 16)
 {
-    Console.WriteLine($"you rolled triple! +6 bonus to total");
-    total += 6;
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You Win!");
+    Console.WriteLine("You win a new laptop!");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-    Console.WriteLine($"You Lose!");
-    
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
